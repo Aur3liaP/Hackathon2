c6de6f56a470fc57d8f0727e86f0c5d2df54efa6
Lorys Lellouche <lorys.lellouche@gmail.com>
2024-07-25 17:52:45 +0200

    merge with dev

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom"
 import "../styles/App.css"
+import Navbar from "./Navbar"
 
 function App() {
   return (
     <>
       <Outlet />
+      <Navbar />
     </>
   )
 }
