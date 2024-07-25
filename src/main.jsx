c9a98f97a2cb0bf3b2ac66e89e1bca28cf2c789e
ThomasDziurdzi <thomasdziurdzi@gmail.com>
2024-07-25 15:20:00 +0200

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,47 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter,RouterProvider } from 'react-router-dom';
-import './index.css'
-import App from "./components/App";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import axios from "axios"
+import Home from "./pages/Home"
+import ProductShowcase from "./pages/ProductShowcase"
+import ProductList from "./pages/ProductList"
+import Basket from "./pages/Basket"
+import "./index.css"
+import App from "./components/App"
 
+const getData = async uri => {
+  const result = await axios.get(uri)
+  return result.data
+}
 
-const router = createBrowserRouter ([
+const router = createBrowserRouter([
   {
-    path: '/',
-    element: <App/>,
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/product-list",
+        element: <ProductList />,
+        loader: () => getData("http://localhost:3310/items")
+      },
+      {
+        path: "/product-showcase/:id",
+        element: <ProductShowcase />,
+        loader: ({params}) => getData(`http://localhost:3310/items/${params.id}`)
+      },
+      {
+        path: "/basket",
+        element: <Basket />,
+      },
+    ],
   },
-
 ])
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
