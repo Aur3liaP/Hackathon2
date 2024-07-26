c4a161082ed3ceb26489503ef94e3f303674641c
Pic Aurélia <pic.aurelia@gmail.com>
2024-07-26 11:56:46 +0200

    Create Responsive cart

--- src/components/CartSummary.jsx
+++ src/components/CartSummary.jsx
@@ -1,20 +1,20 @@
 import RondSoleilSVG from "./svg/RondSoleilSVG"
 import "./styles/CartSummary.css"
-import { useMediaQuery } from "react-responsive"
+import { useState } from "react";
+
 
 function CartSummary() {
 
-    const isDesktopOrLaptop = useMediaQuery({
-        query: "(min-width: 900px)",
-      })
-      const isMobile = useMediaQuery({
-        query: "(max-width: 899px)",
-      })
+    const [code, setCode] = useState('');
+
+    const handleChange = (event) => {
+          setCode(event.target.value);
+      };
 
     return (
       <section className="cartSummary__container">
         <RondSoleilSVG className="homehero__sun-SVG" />
-        {isDesktopOrLaptop && (
+
         <div className="cartSummary">
             <div className="cartSummary__text">
                 <div className="cartSummary__prices">
@@ -32,7 +32,8 @@ function CartSummary() {
                     <hr className="cartSummary__hr"/> 
                         <div className="cartSummary__code">
                             <p className="cartSummary__p"> Code Promo</p>
-                            <input type="text" value="taper le code"/>
+                            <input id="code" type="text" value={code} onChange={handleChange} placeholder="Entrer le code"/>
+
                     </div>
                 </div>
                 
@@ -49,43 +50,6 @@ function CartSummary() {
                 <a href="https://buy.stripe.com/test_7sI28W5RQdl5crSdQR" target="blank">Commander</a>
             </div>
         </div>
-        )}
-        {isMobile && (
-            <div className="cartSummary">
-            <div className="cartSummary__text">
-                <div className="cartSummary__prices">
-                    <h2 className="cartSummary__h2">Prix</h2>
-                    <hr className="cartSummary__hr"/> 
-                        <div className="cartSummary__price">
-                            <span className="cartSummary__span">Sous-total</span>
-                            <p className="cartSummary__p">899 F</p>
-                        </div>
-                        <hr className="cartSummary__hr"/> 
-                        <div className="cartSummary__price">
-                            <span className="cartSummary__span">T.V.A</span>
-                            <p className="cartSummary__p">19 F</p>
-                        </div>
-                    <hr className="cartSummary__hr"/> 
-                        <div className="cartSummary__code">
-                            <p className="cartSummary__p"> Code Promo</p>
-                            <input type="text" value="taper le code"/>
-                    </div>
-                </div>
-                
-                
-                <div className="cartSummary__totaltext">
-                <hr className="cartSummary__hr"/> 
-                    <div className="cartSummary__total">
-                        <h2 className="cartSummary__h2">Total</h2>
-                        <p className="cartSummary__p">899 F</p>
-                    </div>
-                </div>
-            </div>
-            <div className="cartSummary__order">
-                <a href="https://buy.stripe.com/test_7sI28W5RQdl5crSdQR" target="blank">Commander</a>
-            </div>
-        </div>
-        )}
       </section>
     )
   }
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,12 +1,16 @@
 import BasktetProductCard from "../components/BasketProductCard"
 import CartSummary from "../components/CartSummary"
+import Navbar from "../components/Navbar"
 import "./styles/Cart.css"
 
 function Cart() {
     return (
       <main className="cart">
+        <Navbar/>
         <div className="cart_container">
-        <BasktetProductCard/>
+        <div className="cart_productContainer">
+          <BasktetProductCard/>
+        </div>
         <CartSummary/>
         </div>
       </main>
