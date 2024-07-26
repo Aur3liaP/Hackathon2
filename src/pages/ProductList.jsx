8297d10a7851158b993a378a97309ccec64c62d4
Jordan Thivault <jordan.thivault@gmail.com>
2024-07-26 10:28:34 +0200

    lien ok listpdt et shocase - responsive ok commentaire

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,11 +1,18 @@
 import { useLoaderData } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import "./styles/ProductList.css"
 import ProductListProductCard from "../components/ProductListProductCard"
 import Navbar from "../components/Navbar"
 
 function ProductList() {
   const items = useLoaderData()
+  const navigate = useNavigate()
   console.info(items)
+
+  const handleCardClick = (id) => {
+    window.scrollTo(0, 0)
+    navigate(`/product-showcase/${id}`)
+  }
 
   return (
     <>
@@ -16,7 +23,7 @@ function ProductList() {
           
             <div className="product__list__products__wrapper">
               {items.map(item => (
-                <ProductListProductCard key={item.id} item={item} />
+                <ProductListProductCard key={item.id} item={item} onClick={() => handleCardClick(item.id)}/>
               ))}
             </div>
           
