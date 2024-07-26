2a98bb797fba80b9922da5196c8918429ae9d1ec
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-07-26 09:42:23 +0200

    fix broken navbar in cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,16 +1,18 @@
 import BasktetProductCard from "../components/BasketProductCard"
 import CartSummary from "../components/CartSummary"
 import "./styles/Cart.css"
+import Navbar from "../components/Navbar"
 
 function Cart() {
-    return (
-      <main className="cart">
-        <div className="cart_container">
-        <BasktetProductCard/>
-        <CartSummary/>
-        </div>
-      </main>
-    )
-  }
-  
-  export default Cart
+  return (
+    <main className="cart">
+      <Navbar />
+      <div className="cart_container">
+        <BasktetProductCard />
+        <CartSummary />
+      </div>
+    </main>
+  )
+}
+
+export default Cart
